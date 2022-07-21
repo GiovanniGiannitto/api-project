dab9941609b1bd4d44355e3440fae62ede67ad45
Giovanni Giannitto <97680539+GiovanniGiannitto@users.noreply.github.com>
2022-07-21 20:19:16 +0200

    Update planet record with photo filename

--- src/server.test.ts
+++ src/server.test.ts
@@ -3,6 +3,7 @@ import supertest from "supertest";
 import { prismaMock } from "./lib/prisma/client.mock";
 
 import app from "./app";
+import { response } from "express";
 
 const request = supertest(app);
 
@@ -262,6 +263,19 @@ describe("POST /planets/:id/photo", () => {
             .attach("photo", "photos/file.png")
             .expect(201)
             .expect("Access-Control-Allow-Origin", "http://localhost:8080");
+    });
+
+    test("Planet does not exist", async () => {
+        // @ts-ignore
+        prismaMock.planet.update.mockRejectedValue(new Error("Error"));
+
+        const response = await request
+            .post("/planets/23/photo")
+            .attach("photo", "photos/file.png")
+            .expect(404)
+            .expect("Content-Type", /text\/html/);
+
+        expect(response.text).toContain("Cannot POST /planets/23/photo");
     });
 
     test("Invalid planet ID", async () => {
