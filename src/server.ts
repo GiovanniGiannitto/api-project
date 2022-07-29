ce52feb80512e0a1423fe5c94f12d7c1a623da96
Giovanni Giannitto <97680539+GiovanniGiannitto@users.noreply.github.com>
2022-07-29 17:30:29 +0200

    Move all configuration into one script

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from "./config";
 
 import app from "./app";
 
-const port = process.env.PORT;
+const port = config.PORT;
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
